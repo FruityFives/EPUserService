d867c83f61bf1bf298de93183e0d60f5d5440466
abdurrahmanahmed <eaa23aaa@students.eaaa.dk>
2025-05-25 13:00:28 +0200

    okay

--- UserServiceAPI/Services/IUserServiceMongo.cs
+++ UserServiceAPI/Services/IUserServiceMongo.cs
@@ -5,6 +5,6 @@ namespace UserServiceAPI.Services;
 public interface IUserServiceMongo
 {
     Task<User> CreateUser(User user);
-    // Task<ValidatedUserResponse?> ValidateLogin(Login login);
+    // Task<ValidatedUserResponse?> ValidateLogin(Login login); ss
     Task<object?> ValidateLogin(Login login);
 }
